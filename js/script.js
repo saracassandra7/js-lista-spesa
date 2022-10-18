79f108bf376decc6bbac8c1f7abb51ded20c5499
saracassandra7 <saracassandra7@gmail.com>
2022-10-18 17:52:42 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*Abbiamo un array di alimenti.
+Dobbiamo stampare in pagina una lista contenente tutti gli alimenti.
+Stampare sulla pagina gli elementi della lista individualmente con un ciclo for e con un ciclo while
+Consigli:
+- Ricordiamoci di inizializzare la variabile di contatore prima dell’inizio del ciclo while
+- Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all’interno ciclo while*/
+
+console.log('ciao');
